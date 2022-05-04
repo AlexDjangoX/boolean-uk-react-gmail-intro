dfd0539d1ee1b5188195ea0bd4edf42aba2cd66d
AlexDjangoX <alexmonk17@gmail.com>
2022-05-04 15:26:30 +0200

    seperated into components - next seperate css

---
+++ src/AppWrapper.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppWrapper = (props) => {
+  return <div className="app">{props.children}</div>;
+};
+
+export default AppWrapper;
---
+++ src/MainWrapper.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainWrapper = (props) => {
+  return <main className="email-view">{props.children}</main>;
+};
+
+export default MainWrapper;
